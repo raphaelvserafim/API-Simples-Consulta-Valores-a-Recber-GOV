43e30684bf11b480f7729c1af3ee585052ea8051
Raphael Serafim <68257896+raphaelvserafim@users.noreply.github.com>
2022-02-23 00:54:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ use Cachesistemas\Gov\Gov;
 
 
 $gov = new Gov();
-$gov->cpf_cnpj = '258.638.356-72'; // CPF ou CNPJ 
-$gov->data     = '1953-10-09'; // DATA DE NASCIMENTO ou DATA DE ABERTURA EMPRESA YYYY-MM-DD 
+$gov->cpf_cnpj = ''; // CPF ou CNPJ 
+$gov->data     = ''; // DATA DE NASCIMENTO ou DATA DE ABERTURA EMPRESA YYYY-MM-DD 
  $consulta      = $gov->consultar();
 
   echo json_encode($consulta);
